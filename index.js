3830c45d00d9a36ff9423c76f4b14d9f04b0ac0d
Win Feigle <win.feigle@gmail.com>
2022-05-18 11:03:24 -0600

    commit tree 550ee634512a4d49537f61d279a0e282261dbdb3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const saturdayFun = (activity = 'roller-skate') => {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (activity = 'go to the office') => {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (initializer = '') => {
+    const innerFunction = function (adjective) {
+        return `You are ${initializer}${adjective}${initializer}!`;
+    };
+    return innerFunction;
+  }
+  
